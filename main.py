2034f536ae8063c825bc06d9d7717c555a7bc437
Karthik Nair <124065232+karthiknair-19@users.noreply.github.com>
2024-01-17 20:59:53 +0530

    final update

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 
 from dotenv import load_dotenv
-load_dotenv()  # take environment variables from .env (especially openai api key)
+load_dotenv()  
 
-st.title("Alexy:Ask Your URL")
-st.sidebar.title("Article URLs")
+st.title("Alexy The Summarizer")
+st.sidebar.title("News Article URLs")
 
 urls = []
 for i in range(3):
@@ -24,52 +24,43 @@ process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_vector_store.pkl"
 
 main_placeholder = st.empty()
-llm = OpenAI(temperature=0.9, max_tokens=500)
+llm = OpenAI(temperature=0.6, max_tokens=500,model='gpt-3.5-turbo-instruct')
 
 if process_url_clicked:
-    # load data
+    
     loader = UnstructuredURLLoader(urls=urls)
     main_placeholder.text("The data has started loading ...✅✅✅")
     data = loader.load()
-    # split data
+    
     text_splitter = RecursiveCharacterTextSplitter(
         separators=['\n\n', '\n', '.', ','],
         chunk_size=1000
     )
     main_placeholder.text("The data is being splitted into chunks... ⚙️⚙️⚙️")
-    docs = text_splitter.split_documents(data)
-    # create embeddings and save it to FAISS index
+    texts = text_splitter.split_documents(data)
+    
     embeddings = OpenAIEmbeddings()
-    text_embeddings = embeddings.embed_documents(docs)
-    text_embedding_pairs = zip(docs, text_embeddings)
-    text_embedding_pairs_list = list(text_embedding_pairs)
-    vectorstore_openai = FAISS.from_documents(text_embedding_pairs_list, embeddings)
+    faiss=FAISS.from_documents(texts, embeddings)
     main_placeholder.text("Embeddings are getting stored.....📩📩📩")
     time.sleep(2)
-
-    # Save the FAISS index to a pickle file
-    with open(file_path, "wb") as f:
-        pickle.dump(vectorstore_openai, f)
+    faiss.save_local(file_path)
+    
 
 question = main_placeholder.text_input("Question: ")
 if question :
     if os.path.exists(file_path):
-        with open(file_path, "rb") as f:
-            vectordb = pickle.load(f)
+            vectordb=FAISS.load_local(file_path,OpenAIEmbeddings())
             chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectordb.as_retriever())
             result = chain({"question": question}, return_only_outputs=True)
-            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
             st.header("Answer")
             st.write(result["answer"])
 
-            # Display sources, if available
             sources = result.get("sources", "")
             if sources:
                 st.subheader("Sources:")
-                sources_list = sources.split("\n")  # Split the sources by newline
+                sources_list = sources.split("\n")  
                 for source in sources_list:
                     st.write(source)
-
 
 
 
